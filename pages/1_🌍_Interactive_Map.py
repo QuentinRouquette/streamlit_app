bffb3d24f1c06a9732d06b0e6195bcc56635d744
QuentinRouquette <146650587+QuentinRouquette@users.noreply.github.com>
2024-12-02 16:39:39 +0100

    Update 1_🌍_Interactive_Map.py

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -43,22 +43,11 @@ if not substations.empty:
 # Afficher la carte sur Streamlit
 m.to_streamlit(height=700)
 
-# Télécharger les données sous forme de GeoJSON, GeoPackage, GeoParquet ou KML
-@st.cache
-def convert_to_geojson(gdf):
-    return gdf.to_json()
-
+# Télécharger les données sous forme de GeoPackage ou KML
 @st.cache
 def convert_to_geopackage(gdf, place_name):
     buffer = io.BytesIO()
     gdf.to_file(buffer, driver="GPKG", layer=place_name)
-    buffer.seek(0)
-    return buffer.read()
-
-@st.cache
-def convert_to_geoparquet(gdf, place_name):
-    buffer = io.BytesIO()
-    gdf.to_parquet(buffer)
     buffer.seek(0)
     return buffer.read()
 
@@ -69,7 +58,7 @@ def convert_to_kml(gdf, place_name):
     buffer.seek(0)
     return buffer.read()
 
-# Créer un bouton pour télécharger les données OSM en GeoJSON, GeoPackage, GeoParquet ou KML
+# Créer un bouton pour télécharger les données OSM en GeoPackage ou KML
 st.subheader("Télécharger les données")
 
 # Fusionner les données des lignes et sous-stations dans un seul GeoDataFrame
@@ -77,31 +66,15 @@ if not power_lines.empty or not substations.empty:
     all_data = power_lines.append(substations)
     
     # Choisir le format de téléchargement
-    download_format = st.radio("Choisir le format de téléchargement", ("GeoJSON", "GeoPackage", "GeoParquet", "KML"))
+    download_format = st.radio("Choisir le format de téléchargement", ("GeoPackage", "KML"))
     
-    if download_format == "GeoJSON":
-        geojson_data = convert_to_geojson(all_data)
-        st.download_button(
-            label="Télécharger en GeoJSON",
-            data=geojson_data,
-            file_name=f"reseau_electrique_{place_name.replace(' ', '_')}.geojson",
-            mime="application/geo+json"
-        )
-    elif download_format == "GeoPackage":
+    if download_format == "GeoPackage":
         geopackage_data = convert_to_geopackage(all_data, place_name)
         st.download_button(
             label="Télécharger en GeoPackage",
             data=geopackage_data,
             file_name=f"reseau_electrique_{place_name.replace(' ', '_')}.gpkg",
             mime="application/geopackage+sqlite3"
-        )
-    elif download_format == "GeoParquet":
-        geoparquet_data = convert_to_geoparquet(all_data, place_name)
-        st.download_button(
-            label="Télécharger en GeoParquet",
-            data=geoparquet_data,
-            file_name=f"reseau_electrique_{place_name.replace(' ', '_')}.parquet",
-            mime="application/octet-stream"
         )
     elif download_format == "KML":
         kml_data = convert_to_kml(all_data, place_name)
